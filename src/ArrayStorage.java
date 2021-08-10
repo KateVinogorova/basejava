4fba0d1ca778ef42de164cf6e647ee6adee9f899
Ekaterina Vinogorova <katarina_gvozdik@mail.ru>
2021-08-10 14:43:50 +0300

    All methods are implemented except of Delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,15 +3,33 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int size = 0;
 
     void clear() {
+        for(int i = 0; i < size; i++){
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(r.uuid)){
+                System.out.println("Resume with uuid " + r.uuid + "is already exist");
+                return;
+            }
+            else {
+                storage[size] = r;
+                size++;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume targetResume = null;
+        for (Resume r : storage) {
+            if (r.uuid.equals(uuid)) targetResume = r;
+        }
+        return targetResume;
     }
 
     void delete(String uuid) {
@@ -21,10 +39,24 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResumes;
+        if (size == 0) {
+            allResumes = null;
+            System.out.println("Storage is empty");
+        }
+        else {
+            allResumes = new Resume[size];
+            for (int i = 0; i < size; i++) {
+                allResumes[i] = storage[i];
+            }
+            for(Resume r : allResumes) {
+                System.out.println(r);
+            }
+        }
+        return allResumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
